72b313de54710572fd7648c091c182ad29d764fe
aozaki-kuro <aozaki.kuro@gmail.com>
2022-02-19 16:52:52 +0800

    fix twitter icon problem

--- src/components/SNSIcon.jsx
+++ src/components/SNSIcon.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
 const SNSIcon = function (prop) {
-  let Name = prop.Name || ''
-  if (Name == 'twitter') {
-    return <i className="fa-brands fa-twitter"></i>
+  const Name = prop.Name || ''
+  if (Name == `twitter`) {
+    return <i className='fa-brands fa-twitter'></i>
   } else {
     return <i className="fa-brands fa-youtube"></i>
   }
