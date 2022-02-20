73e368369f7e8e58592f9ec1d341d3af8e19c9a7
Aozaki <aozaki.kuro@gmail.com>
2022-02-20 08:41:26 +0800

    fix const name

--- src/components/YouTubeEmbed.jsx
+++ src/components/YouTubeEmbed.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import ReactPlayer from 'react-player'
+import ReactPlayer from 'react-player/lazy'
 import '@site/src/css/video.css'
 
 function YTEmbed(props) {
   const Id = props.Id
-  const Url = `https://www.youtube-nocookie.com/watch?v=${props.Id}`
+  const Url = `https://www.youtube-nocookie.com/watch?v=${Id}`
   return (
     <div className="video-responsive">
       <ReactPlayer url={Url} controls="true" volume="0.5" />
