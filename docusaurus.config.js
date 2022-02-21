7cd6d3da3aa8815768267b7acb9c79a2c29bde98
aozaki-kuro <aozaki.kuro@gmail.com>
2022-02-21 09:59:05 +0800

    try if twitter:card conflict with page image

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -84,11 +84,10 @@ const config = {
     ({
       // metadata
       metadata: [
-        /** 
         {
           name: 'twitter:card',
-          content: '/img/android-chrome-192x192.png',
-        },*/
+          content: 'https://img.suisei.cc/wiki/icon.png',
+        },
         /**
         {
           name: 'twitter:site',
