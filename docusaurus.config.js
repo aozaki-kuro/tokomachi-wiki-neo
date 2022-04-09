e814b7a6e8ca6902c89d635e200b406dc6d409ca
Aozaki <aozaki.kuro@gmail.com>
2022-04-09 21:02:41 +0800

    update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -17,6 +17,24 @@ const config = {
   organizationName: 'aozaki-kuro', // Usually your GitHub org/user name.
   projectName: 'tokomachi-wiki-neo', // Usually your repo name.
 
+  webpack: {
+    jsLoader: (isServer) => ({
+      loader: require.resolve('swc-loader'),
+      options: {
+        jsc: {
+          "parser": {
+            "syntax": "typescript",
+            "tsx": true
+          },
+          target: 'es2017',
+        },
+        module: {
+          type: isServer ? 'commonjs' : 'es6',
+        }
+      },
+    }),
+  },
+
   presets: [
     [
       'classic',
