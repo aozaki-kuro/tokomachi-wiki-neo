4d9666e0ca5dedfbfc19611909aea126d41ff6ca
Aozaki <aozaki.kuro@gmail.com>
2022-05-14 13:39:26 +0800

    add zhtw translation

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -27,10 +27,12 @@ const config = {
       // You can omit a locale (e.g. fr) if you don't need to override the defaults
       'zh-TW': {
         htmlLang: 'zh-TW',
+        label: '繁體中文',
       },
       'zh-CN': {
         htmlLang: 'zh-CN',
-      }
+        label: '简体中文',
+      },
     },
   },
 
