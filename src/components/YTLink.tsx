2b91334081a3dd4d04fb20c10176024c11d56b13
Aozaki <aozaki.kuro@gmail.com>
2022-05-15 17:27:06 +0800

    add translation to components

--- src/components/YTLink.tsx
+++ src/components/YTLink.tsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import Link from '@docusaurus/Link'
 
+import Translate from '@docusaurus/Translate'
+
 function YTLink(props) {
   const Id = props.Id
   const Url = `https://youtu.be/${Id}`
   return (
     <Link to={Url}>
-      <i className="fa-brands fa-youtube"></i> Original YouTube Stream
+      <i className="fa-brands fa-youtube"></i>{' '}
+      <Translate id="YTLink.text" description="YouTube link text">
+        Original YouTube Stream
+      </Translate>
     </Link>
   )
 }
